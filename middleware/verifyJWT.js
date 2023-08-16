2855f56505cfcf6fd1b2282b33942a88e8b640b2
sebastianiv21 <seluti98@gmail.com>
2023-08-16 13:38:30 -0500

    feat: agregar middleware de autorizacion

---
+++ middleware/verifyJWT.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+// TODO: apply this middleware to all routes that need to be protected (tests)
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+
+  if (!authHeader?.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No autorizado' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ message: 'No autorizado' });
+
+    req.email = decoded.userInfo.email;
+    req.roles = decoded.userInfo.roles;
+    next();
+  });
+};
+
+module.exports = verifyJWT;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const usersController = require('../controllers/usersController');
 
 const router = express.Router();
+const usersController = require('../controllers/usersController');
+const verifyJWT = require('../middleware/verifyJWT');
+
+// router.use(verifyJWT); // TODO: aplicar este mismo middleware a tests y citas
 
 router
   .route('/')
-  .get(usersController.getAllUsers)
-  .post(usersController.createUser);
+  .get(verifyJWT, usersController.getAllUsers)
+  .post(verifyJWT, usersController.createUser);
 router
   .route('/:id')
-  .patch(usersController.updateUser)
-  .delete(usersController.deleteUser);
+  .patch(verifyJWT, usersController.updateUser)
+  .delete(verifyJWT, usersController.deleteUser);
 router.post('/create-admin', usersController.createAdmin);
 
 module.exports = router;
