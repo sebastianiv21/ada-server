050c4dc2eb2ea9318f6c714b1acdb8550620daf7
sebastianiv21 <seluti98@gmail.com>
2023-11-05 14:06:04 -0500

    feat: create admin validation

--- controllers/lugaresController.js
+++ controllers/lugaresController.js
@@ -1,12 +1,25 @@
 import { jsonResponse } from '#utils';
 import services from '#services/lugaresServices.js';
 
+/**
+ * @desc Obtiene los departamentos
+ * @param {Object} req Request
+ * @param {Object} res Response
+ * @returns {Object} Response
+ */
 const getDepartamentos = async (req, res) => {
   const { skip, limit } = req.query;
   const departamentos = await services.getDepartamentos(skip, limit);
+
   return jsonResponse(res, departamentos, 200);
 };
 
+/**
+ * @desc Obtiene los municipios por departamento id
+ * @param {Object} req Request
+ * @param {Object} res Response
+ * @returns {Object} Response
+ */
 const getMunicipiosPorDepartamentoId = async (req, res) => {
   const { skip, limit } = req.query;
   const { departamentoId } = req.params;
--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,23 @@
 import { jsonResponse } from '#utils';
 
 import services from '#services/usuarioServices.js';
+
+/**
+ * @route   POST /users/crear-admin
+ * @desc Crea un usuario administrador si no existe ninguno
+ * @param {Object} req - Request Object
+ * @param {Object} res - Response Object
+ * @return {Object} - Response Object
+ */
+const createAdmin = async (req, res) => {
+  // valida que no exista un administrador
+  const admin = await services.findAdmin();
+
+  if (admin) {
+    return jsonResponse(res, { message: 'Ya existe un administrador' }, 409); // 409 Conflict
+  }
+
+  return jsonResponse(res, { message: 'No existe un administrador' }, 404); // 404 Not Found
+};
+
+export default { createAdmin };
