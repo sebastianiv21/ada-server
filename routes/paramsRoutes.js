fe7aaa93acd58725f556cf85f667b7bb6536e2ff
sebastianiv21 <seluti98@gmail.com>
2023-12-01 22:58:55 -0500

    chore(params): delete params routes protection

--- routes/paramsRoutes.js
+++ routes/paramsRoutes.js
@@ -1,10 +1,7 @@
 import { Router } from 'express';
 import controller from '#controllers/paramsController.js';
-import verifyJWT from '#middlewares/verifyJWT.js';
 
 const router = Router();
-
-router.use(verifyJWT);
 
 router.route('/').get(controller.getParams);
 
