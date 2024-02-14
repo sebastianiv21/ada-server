4321de42923e9d80fdc1a421784dc8559eeb1b43
sebastianiv21 <seluti98@gmail.com>
2024-02-13 19:51:39 -0500

    feat(dbConn): add local database connection

--- config/dbConn.js
+++ config/dbConn.js
@@ -9,11 +9,17 @@ const USER = process.env.DATABASE_USER;
 const PWD = process.env.DATABASE_PASSWORD;
 const HOST = process.env.DATABASE_HOST;
 const DATABASE = process.env.DATABASE_NAME;
+const LOCAL_DATABASE = process.env.DATABASE_LOCAL_URI;
 
-const DATABASE_URI = `mongodb+srv://${USER}:${PWD}@${HOST}/${DATABASE}?retryWrites=true&w=majority`;
+const isLocal = [USER, PWD, HOST].some((value) => !value);
+
+const DATABASE_URI = isLocal
+  ? `${LOCAL_DATABASE}/${DATABASE}`
+  : `mongodb+srv://${USER}:${PWD}@${HOST}/${DATABASE}?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
   try {
+    if (!DATABASE) throw new Error('No se ha definido la base de datos');
     await mongoose.connect(DATABASE_URI);
   } catch (error) {
     console.error(error);
